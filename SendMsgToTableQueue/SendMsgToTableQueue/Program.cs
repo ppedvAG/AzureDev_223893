24375a0683e3b52f8ba2d115ef9f4f1b4bec00c1
André <AndreR@ppedv.de>
2023-01-17 13:43:34 +0100

    Azure Msg Sender

---
+++ SendMsgToTableQueue/SendMsgToTableQueue/Program.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Storage.Queues;
+
+Console.WriteLine("*** Azure Queue Sender ***");
+
+var conString = "DefaultEndpointsProtocol=https;AccountName=mystoragefun;AccountKey=HvsTakf4kCu47HQjIbjUDIbrKV82LCHgPDXEejA2SP0TyVDkTej49fqK80c/lvVFCr/mTDzw/ZeD+AStMsoCWw==;EndpointSuffix=core.windows.net";
+var queueName = "zeug";
+
+var client = new QueueClient(conString, queueName);
+
+
+for (int i = 0; i < 10; i++)
+{
+    client.SendMessage($"Hallo {DateTime.Now:G}");
+}
